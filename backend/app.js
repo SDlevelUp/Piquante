86154688f2d04fbfc2f410db4cf31fc92e1dda86
Sarah Berri <sarahberri@outlook.fr>
2022-11-22 15:21:46 +0100

    commit changes

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+
+// Path : donne accés au chemin du système de fichiers
 const path = require('path');
+
+// Helmet : protéger votre application de certaines vulnérabilités, 
+// => il ajoute des en-têtes HTTP qui empêchent le détournement d’informations.
 const helmet = require('helmet');
 
-//Dotenv = Importer un fichier de variables d'environnement.
-const dotenv = require("dotenv").config();
+//Dotenv = Importer un fichier de variables d'environnement
+// const dotenv = require("dotenv").config();
 
-const bodyParser = require('body-parser');
-
-
-// importation de mongo-sanitize pour éviter les injections SQL
+// Sanitize = Il supprimera toutes les clés commençant par '$', ainsi assainir votre code
 const sanitize = require('express-mongo-sanitize');
 
 // Déclaration des routes à utilisées
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 
-// ELABORATION DE L'APP EXPRESS
+// App Express (framework backend)
 const app = express();
 
-//RATE-LIMIT
+//RateLimit : limiter les demandes répétées aux API publiques et/ou aux points de terminaison 
+// => comme les demandes de réinitialisation du MDP
 const rateLimit = require('express-rate-limit')
+// apiLimiter = Configuration de la limite des requêtes autorisées par fenêtres
 const apiLimiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
 	max: 100, // Limite chaque IP à 100 requêtes par fenêtre toutes les 15 minutes
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	standardHeaders: true, // Renvoyer les informations de limite de taux dans les en-têtes `RateLimit-*`
+	legacyHeaders: false, // Désactiver les en-têtes `X-RateLimit-*
 })
 
 // Connexion à mongoDB
-mongoose.connect(process.env.MONGODB_URI,
+mongoose.connect(
+  "mongodb+srv://Sarah:Michmich-91@cluster0.p3w5kdv.mongodb.net/?retryWrites=true&w=majority",
   { useNewUrlParser: true,
       useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -54,10 +60,8 @@ app.use((req, res, next) => {
   next();
 });
 
-// Conversion en JSON: intercepte les requête 'content-typeJSON'
-// Ici, ajout des middleware des fichiers : user, sauces et images
+
 app.use(express.json());
-app.use(bodyParser.json());
 
 //EXPORTATION DES ROUTES SAUCES, AUTHENTIFICATION ET IMAGE
 app.use('/api/sauces', saucesRoutes);
@@ -70,7 +74,7 @@ app.use(helmet());
 // Ajout du middleware associé à express-rate-limit
 app.use('/api', apiLimiter)
 
-//Ajout du middleware sanitize
+//Ajout du middleware associé à MongoSanitize
 app.use(sanitize());
 
 module.exports = app;
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,4 @@
-/************************* MIDDLEWARE D'AUTHENTIFICATION *************************/
+/************************* CONTROLLERS / USER *************************/
 
 // On exporte le 'models/User' afin 'd'enregistrer et pouvoir lire' les users
 const User = require('../models/User');
@@ -9,19 +9,23 @@ const jwt = require('jsonwebtoken');
 // Installation de bcrypt : cryptage mdp
 const bcrypt = require('bcrypt');
 
-require('dotenv').config(); 
+// CryptoJS : Module qui aide à chiffrer, déchiffrer ou hacher des données
+const cryptojs = require('crypto-js')
+
+console.log(process.env.CRYPTOJS_EMAIL)
 
 /*************************SIGNUP*************************/
 
 // 1. SIGNUP : qui va créer le mot de passe
 exports.signup = (req, res, next) => {
 
-  // saler la fonction 10x, plus c'est élevé plus cest long et sécurisé => fonction asynchrone
+  const cryptEmail = cryptojs.HmacSHA256(req.body.email, process.env.CRYPTOJS_EMAIL).toString();
+    // saler la fonction 10x, plus c'est élevé plus cest long et sécurisé => fonction asynchrone
   bcrypt.hash(req.body.password, 10)
     .then((hash) => {
       // FABRICATION USER
       const user = new User({
-        email: req.body.email,
+        email: cryptEmail,
         password: hash
       });
       user.save()
@@ -34,7 +38,10 @@ exports.signup = (req, res, next) => {
 /*************************LOGIN*************************/
 // 2. Le LOGIN : qui va vérifier qu l'utilisateur se connecte avec des identifiantes valides
 exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email })
+
+
+    const decryptEmail = cryptojs.HmacSHA256(req.body.email, process.env.CRYPTOJS_EMAIL).toString();
+  User.findOne({ email: decryptEmail })
       .then(user => {
           if (!user) {
               return res.status(401).json({ error: 'Utilisateur non trouvé !' });
